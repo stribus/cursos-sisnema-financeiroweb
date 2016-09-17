c4a1138be400a6962315a9fa8620a04f05aad78a
Flavio Toldo <paladinotoldo@gmail.com>
2016-09-17 14:22:12 -0300

    Regras de negocio

---
+++ src/main/java/br/com/sisnema/financeiroweb/model/Usuario.java
@@ -0,0 +1,225 @@
+package br.com.sisnema.financeiroweb.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.NaturalId;
+
+@Entity
+public class Usuario implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8359745164118178955L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "cod_usuario")
+	private Integer codigo;
+
+	private String nome;
+
+	@NaturalId
+	private String login;
+
+	private String senha;
+
+	private String celular;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date nascimento;
+
+	private String idioma;
+
+	private String email;
+
+	private boolean ativo;
+
+	public Usuario() {
+		super();
+	}
+
+	public Usuario(Integer codigo) {
+		super();
+		this.codigo = codigo;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getCelular() {
+		return celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+	public Date getNascimento() {
+		return nascimento;
+	}
+
+	public void setNascimento(Date nascimento) {
+		this.nascimento = nascimento;
+	}
+
+	public String getIdioma() {
+		return idioma;
+	}
+
+	public void setIdioma(String idioma) {
+		this.idioma = idioma;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (ativo ? 1231 : 1237);
+		result = prime * result + ((celular == null) ? 0 : celular.hashCode());
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((idioma == null) ? 0 : idioma.hashCode());
+		result = prime * result + ((login == null) ? 0 : login.hashCode());
+		result = prime * result + ((nascimento == null) ? 0 : nascimento.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((senha == null) ? 0 : senha.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Usuario)) {
+			return false;
+		}
+		Usuario other = (Usuario) obj;
+		if (ativo != other.ativo) {
+			return false;
+		}
+		if (celular == null) {
+			if (other.celular != null) {
+				return false;
+			}
+		} else if (!celular.equals(other.celular)) {
+			return false;
+		}
+		if (codigo == null) {
+			if (other.codigo != null) {
+				return false;
+			}
+		} else if (!codigo.equals(other.codigo)) {
+			return false;
+		}
+		if (email == null) {
+			if (other.email != null) {
+				return false;
+			}
+		} else if (!email.equals(other.email)) {
+			return false;
+		}
+		if (idioma == null) {
+			if (other.idioma != null) {
+				return false;
+			}
+		} else if (!idioma.equals(other.idioma)) {
+			return false;
+		}
+		if (login == null) {
+			if (other.login != null) {
+				return false;
+			}
+		} else if (!login.equals(other.login)) {
+			return false;
+		}
+		if (nascimento == null) {
+			if (other.nascimento != null) {
+				return false;
+			}
+		} else if (!nascimento.equals(other.nascimento)) {
+			return false;
+		}
+		if (nome == null) {
+			if (other.nome != null) {
+				return false;
+			}
+		} else if (!nome.equals(other.nome)) {
+			return false;
+		}
+		if (senha == null) {
+			if (other.senha != null) {
+				return false;
+			}
+		} else if (!senha.equals(other.senha)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [codigo=" + codigo + ", nome=" + nome + ", login=" + login + ", senha=" + senha + ", celular="
+				+ celular + ", nascimento=" + nascimento + ", idioma=" + idioma + ", email=" + email + ", ativo="
+				+ ativo + "]";
+	}
+
+}
---
+++ src/main/java/br/com/sisnema/financeiroweb/negocio/UsuarioRN.java
@@ -0,0 +1,76 @@
+package br.com.sisnema.financeiroweb.negocio;
+
+import java.util.List;
+
+import br.com.sisnema.financeiroweb.dao.IDAO;
+import br.com.sisnema.financeiroweb.dao.UsuarioDAO;
+import br.com.sisnema.financeiroweb.exception.DAOException;
+import br.com.sisnema.financeiroweb.exception.RNException;
+import br.com.sisnema.financeiroweb.model.Usuario;
+
+public class UsuarioRN extends RN<Usuario> {
+
+	public UsuarioRN() {
+		super(new UsuarioDAO());
+	}
+
+	public void salvar(Usuario usuarioTela) throws RNException {
+
+		try {
+			if (usuarioTela.getCodigo() != null) {
+				// update
+				((UsuarioDAO) dao).atualizar(usuarioTela);
+
+			} else {
+				// insert
+				Usuario usuarioBanco = buscarPorLogin(usuarioTela.getLogin());
+				if (usuarioBanco != null) {
+					throw new RNException("Já existe um usuário com o login informado.");
+				}
+				dao.salvar(usuarioTela);
+			}
+		} catch (DAOException e) {
+			throw new RNException(e.getMessage(), e);
+		}
+
+	}
+
+	public void excluir(Usuario model) throws RNException {
+		try {
+			dao.excluir(model);
+		} catch (DAOException e) {
+			throw new RNException(e.getMessage(), e);
+		}
+	}
+
+	public Usuario obterPorId(Usuario filtro) {
+		return dao.obterPorId(filtro);
+	}
+
+	public List<Usuario> pesquisar(Usuario filtros) {
+		return dao.pesquisar(filtros);
+	}
+
+	public Usuario buscarPorLogin(String login) {
+		// váriavel dao é do tipo IDAO ou seja, pois apenas os métodos
+		// genéricos, logo
+		// para se ter acesso aos métodos específicos de UsuarioDAO temos de
+		// torná-la
+		// mais específica, realizando uma operação de DOWNCASTING.
+		UsuarioDAO userDAO = (UsuarioDAO) dao;
+		try {
+			return userDAO.buscarPorLogin(login);
+		} catch (DAOException e) {
+			return null;
+		}
+	}
+
+	public Usuario buscarPorLoginESenha(String login, String senha) {
+		try {
+			return ((UsuarioDAO) dao).buscarPorLoginESenha(login, senha);
+		} catch (DAOException e) {
+			return null;
+		}
+	}
+
+}
