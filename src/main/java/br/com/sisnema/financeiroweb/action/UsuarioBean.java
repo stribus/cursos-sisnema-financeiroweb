45f0eef2b0b2be4095a7c7494b2fa74c22d8b3f2
Flavio Toldo <paladinotoldo@gmail.com>
2016-09-17 16:37:27 -0300

    Na classe UsuarioBean criar   List<Usuario> lista;        gets/sets   String destinoSalvar;        gets/sets  No getLista efetuar um teste caso a lista esteja nula efetuar a pesquisa via negocio…
     No método novo 
      Atribuir ao atributo destinoSalvar : /publico/usuarioSucesso
      Atribuir ao atributo usuario.ativo para true
    
    
     Criar o método editar 
      Atribuir ao atributo destinoSalvar : /admin/usuarioAdm
      Atribuir ao atributo confirmaSenha o valor da senha do usuario
      Retornar para a página de /publico/usuario
    
    
     No método salvar, 
      Em caso de sucesso atribuir null ao atributo lista
      Retornar o valor do atributo destinoSalvar em caso de sucesso
    
    
     Crie o método: public String ativar(){} 
      Inverta o valor do atributo ativo do objeto usuario
      Atualize o objeto usuario junto a base de dados
      Em caso de sucesso exibir uma mensagem: Usuário ativado/inativado com
    sucesso.
    
    
     Crie o método excluir
      Chamar o método excluir da camada de negócio
      Em caso de sucesso exibir uma mensagem: Usuário excluído com sucesso.
      Em caso de sucesso atribuir null ao atributo lista
    
    No arquivo usuario.xhtml
     Adicione o campo oculto destinoSalvar

--- src/main/java/br/com/sisnema/financeiroweb/action/UsuarioBean.java
+++ src/main/java/br/com/sisnema/financeiroweb/action/UsuarioBean.java
@@ -1,5 +1,7 @@
 package br.com.sisnema.financeiroweb.action;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
@@ -15,29 +17,31 @@ public class UsuarioBean extends ActionBean<Usuario> {
 
 	private Usuario usuario = new Usuario();
 	private String confirmaSenha;
-	
+
+	private List<Usuario> lista;
+	private String destinoSalvar;
+
 	public UsuarioBean() {
 		super(new UsuarioRN());
 	}
 
 	public String salvar() {
-		
+
 		try {
 			if (!StringUtils.equals(usuario.getSenha(), confirmaSenha)) {
 				apresentarMensagemDeErro("Senhas diferentes");
 				return null;
-				
+
 			} else {
-				
-			String msg = "Usuário "
-					+ (usuario.getCodigo() != null ? " alterado " : " inserido ") 
-					+ "com sucesso";
-			
-			negocio.salvar(usuario);
-			
-			apresentarMensagemDeSucesso(msg);
-			
-			return "usuarioSucesso";
+
+				String msg = "Usuário " + (usuario.getCodigo() != null ? " alterado " : " inserido ") + "com sucesso";
+
+				negocio.salvar(usuario);
+
+				apresentarMensagemDeSucesso(msg);
+				lista = null;
+
+				return destinoSalvar;
 
 			}
 		} catch (RNException e) {
@@ -50,7 +54,56 @@ public class UsuarioBean extends ActionBean<Usuario> {
 
 	public String novo() {
 		this.usuario = new Usuario();
+		usuario.setAtivo(true);
+
+		destinoSalvar = "/publico/usuarioSucesso";
+
 		return "usuario";
+	}
+
+
+	public String editar() {
+
+		destinoSalvar = "/admin/usuarioAdm";
+		confirmaSenha = usuario.getSenha();
+		return "/publico/usuario";
+	}
+	
+	public String excluir() {
+		
+		try {			
+			negocio.excluir(usuario);
+			apresentarMensagemDeSucesso("Usuário excluído com sucesso.");
+			lista = null;
+		} catch (RNException e) {			
+			apresentarMensagemDeErro(e);
+		}
+		return null;
+	}
+	
+
+	public String ativar() {
+		
+		try {
+			usuario.setAtivo(!usuario.isAtivo());
+			negocio.salvar(usuario);
+			apresentarMensagemDeSucesso("Usuário "+
+					(usuario.isAtivo() ? "ativado" : "inativado")+
+					" com sucesso!");
+			//lista = null;//nao precisa
+		} catch (RNException e) {
+			usuario.setAtivo(!usuario.isAtivo());
+			apresentarMensagemDeErro(e);
+		}
+		return null;
+	}
+	
+	public List<Usuario> getLista() {
+
+		if (lista == null) {
+			lista = negocio.pesquisar(new Usuario());
+		}
+		return lista;
 	}
 
 	public Usuario getUsuario() {
@@ -67,6 +120,14 @@ public class UsuarioBean extends ActionBean<Usuario> {
 
 	public void setConfirmaSenha(String confirmaSenha) {
 		this.confirmaSenha = confirmaSenha;
+	}
+
+	public String getDestinoSalvar() {
+		return destinoSalvar;
+	}
+
+	public void setDestinoSalvar(String destinoSalvar) {
+		this.destinoSalvar = destinoSalvar;
 	}
 
 }
