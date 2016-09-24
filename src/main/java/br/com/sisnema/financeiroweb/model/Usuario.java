a5185e21f1fff511395801a88b2a8636efef9b38
Flavio Toldo <paladinotoldo@gmail.com>
2016-09-24 13:30:05 -0300

    adicionado permissoes ao usuario

--- src/main/java/br/com/sisnema/financeiroweb/model/Usuario.java
+++ src/main/java/br/com/sisnema/financeiroweb/model/Usuario.java
@@ -1,16 +1,26 @@
 package br.com.sisnema.financeiroweb.model;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
 
 import org.hibernate.annotations.NaturalId;
+
+import br.com.sisnema.financeiroweb.domain.UsuarioPermissao;
 
 @Entity
 public class Usuario extends BaseEntity {
@@ -42,6 +52,24 @@ public class Usuario extends BaseEntity {
 	private String email;
 
 	private boolean ativo;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name="permissao", length=50, nullable=false)
+	@ElementCollection(targetClass=UsuarioPermissao.class)
+	@JoinTable( name = "usuario_permissao",
+				uniqueConstraints = {@UniqueConstraint(columnNames={"usuario", "permissao"})},
+				joinColumns = @JoinColumn(name="usuario")
+			   )
+	private Set<UsuarioPermissao> permissoes = new HashSet<UsuarioPermissao>();
+
+
+	public Set<UsuarioPermissao> getPermissoes() {
+		return permissoes;
+	}
+
+	public void setPermissoes(Set<UsuarioPermissao> permissoes) {
+		this.permissoes = permissoes;
+	}
 
 	public Usuario() {
 		super();
@@ -127,7 +155,7 @@ public class Usuario extends BaseEntity {
 	@Override
 	public int hashCode() {
 		final int prime = 31;
-		int result = 1;
+		int result = super.hashCode();
 		result = prime * result + (ativo ? 1231 : 1237);
 		result = prime * result + ((celular == null) ? 0 : celular.hashCode());
 		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
@@ -136,81 +164,67 @@ public class Usuario extends BaseEntity {
 		result = prime * result + ((login == null) ? 0 : login.hashCode());
 		result = prime * result + ((nascimento == null) ? 0 : nascimento.hashCode());
 		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((permissoes == null) ? 0 : permissoes.hashCode());
 		result = prime * result + ((senha == null) ? 0 : senha.hashCode());
 		return result;
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj) {
+		if (this == obj)
 			return true;
-		}
-		if (obj == null) {
+		if (!super.equals(obj))
 			return false;
-		}
-		if (!(obj instanceof Usuario)) {
+		if (!(obj instanceof Usuario))
 			return false;
-		}
 		Usuario other = (Usuario) obj;
-		if (ativo != other.ativo) {
+		if (ativo != other.ativo)
 			return false;
-		}
 		if (celular == null) {
-			if (other.celular != null) {
+			if (other.celular != null)
 				return false;
-			}
-		} else if (!celular.equals(other.celular)) {
+		} else if (!celular.equals(other.celular))
 			return false;
-		}
 		if (codigo == null) {
-			if (other.codigo != null) {
+			if (other.codigo != null)
 				return false;
-			}
-		} else if (!codigo.equals(other.codigo)) {
+		} else if (!codigo.equals(other.codigo))
 			return false;
-		}
 		if (email == null) {
-			if (other.email != null) {
+			if (other.email != null)
 				return false;
-			}
-		} else if (!email.equals(other.email)) {
+		} else if (!email.equals(other.email))
 			return false;
-		}
 		if (idioma == null) {
-			if (other.idioma != null) {
+			if (other.idioma != null)
 				return false;
-			}
-		} else if (!idioma.equals(other.idioma)) {
+		} else if (!idioma.equals(other.idioma))
 			return false;
-		}
 		if (login == null) {
-			if (other.login != null) {
+			if (other.login != null)
 				return false;
-			}
-		} else if (!login.equals(other.login)) {
+		} else if (!login.equals(other.login))
 			return false;
-		}
 		if (nascimento == null) {
-			if (other.nascimento != null) {
+			if (other.nascimento != null)
 				return false;
-			}
-		} else if (!nascimento.equals(other.nascimento)) {
+		} else if (!nascimento.equals(other.nascimento))
 			return false;
-		}
 		if (nome == null) {
-			if (other.nome != null) {
+			if (other.nome != null)
 				return false;
-			}
-		} else if (!nome.equals(other.nome)) {
+		} else if (!nome.equals(other.nome))
 			return false;
-		}
+		if (permissoes == null) {
+			if (other.permissoes != null)
+				return false;
+		} else if (!permissoes.equals(other.permissoes))
+			return false;
 		if (senha == null) {
-			if (other.senha != null) {
+			if (other.senha != null)
 				return false;
-			}
-		} else if (!senha.equals(other.senha)) {
+		} else if (!senha.equals(other.senha))
 			return false;
-		}
 		return true;
 	}
 
